2b3493afbe0474fc0a4c6d0c2f93f305b10807b8
Jesse Gibson <JesseTheGibson@gmail.com>
2025-03-19 11:22:54 -0400

    Create stub for AFrame mount point

---
+++ src/components/app/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const App = () => {
+  // TODO: Mount a-frame here.
+  return null;
+};
---
+++ src/pages/app.tsx
@@ -0,0 +1,6 @@
+import { HeadFC } from 'gatsby';
+import React from 'react';
+
+export const Head: HeadFC = () => <title>VR Development Environment</title>;
+
+export { App as default } from '../components/app/App';
