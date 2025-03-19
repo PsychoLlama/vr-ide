4a748284f3a39cf59d1174f6260fec53c98d8428
Jesse Gibson <JesseTheGibson@gmail.com>
2025-03-19 11:31:45 -0400

    Create hello-world AFrame app

--- src/components/app/App.css.ts
+++ src/components/app/App.css.ts
@@ -1,5 +1,9 @@
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
-  // TODO
+  width: '100%',
+  height: '100vh',
+  margin: 0,
+  padding: 0,
+  overflow: 'hidden'
 });
--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
+import 'aframe';
+import { container } from './App.css';
 
 export const App = () => {
-  // TODO: Mount a-frame here.
-  return null;
+  return (
+    <div className={container}>
+      <a-scene embedded>
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9" />
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E" />
+        <a-cylinder
+          position="1 0.75 -3"
+          radius="0.5"
+          height="1.5"
+          color="#FFC65D"
+        />
+        <a-plane
+          position="0 0 -4"
+          rotation="-90 0 0"
+          width="4"
+          height="4"
+          color="#7BC8A4"
+        />
+        <a-sky color="#ECECEC" />
+        <a-text
+          value="Hello, A-Frame!"
+          position="0 2 -3"
+          color="#000000"
+          align="center"
+        />
+      </a-scene>
+    </div>
+  );
 };
