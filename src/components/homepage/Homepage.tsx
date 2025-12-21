677e61f357341e6e18a107d86d72e11aed87798a
Jesse Gibson <JesseTheGibson@gmail.com>
2025-12-21 15:26:17 -0500

    Remove unused Homepage component

--- src/components/homepage/Homepage.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as styles from './Homepage.css';
-
-export const Homepage: React.FC = () => (
-  <main className={styles.container}>
-    <h1>Hello, world!</h1>
-  </main>
-);
